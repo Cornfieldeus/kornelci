61fe770f119abfff078591b2f1040e8879875897
kornel koteles <koteleskornel@gmail.com>
2024-10-03 09:18:52 +0000

    Linting and unit tests fix

--- exercise_2/bad_code.py
+++ exercise_2/bad_code.py
@@ -1,7 +1,6 @@
 def basic_math():
     return 2 + 3
-# A comment that is so long, it is going to be split into multiple lines due for general readability purposes
+#toolong
 if __name__== "__main__":
     hello = "Hello, World!"
     basic_math()
-
